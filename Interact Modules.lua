95653379eeb83655b95e349b1619cc3d29a8e14b
KmeCP <79467385+KmeCP@users.noreply.github.com>
2021-12-20 21:34:15 -0600

    Create Interact Modules.lua

---
+++ Interact Modules.lua
@@ -0,0 +1,11 @@
+local module = {}
+
+local trys = 0
+
+module.Tip = "Text about what object "
+
+module.Interact = function()
+	--function of what happens when u press e on object
+end
+
+return module
